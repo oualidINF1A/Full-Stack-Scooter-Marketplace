8831b2cda4de8fd357d23145dd3a21c2850ba6b3
oualid <95184289+oualidINF1A@users.noreply.github.com>
2023-03-28 19:03:23 +0200

    Render link replaced the old localhost link in client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,8 @@ import CategorySelectedPage from './pages/CategorySelectedPage'
 import QueryPage from './pages/QueryPage'
 
 
-axios.defaults.baseURL = 'http://localhost:3030/'
+//axios.defaults.baseURL = 'http://localhost:3030/'
+axios.defaults.baseURL = 'https://scouterscout.onrender.com/'
 axios.defaults.withCredentials = true
 
 function App() {
